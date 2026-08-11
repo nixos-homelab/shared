be11987cd861ba80b39a18fd8b5f1af980c69eea
Anders Ingemann <anders@ingemann.de>
2026-08-11 21:07:46 +0200

    Expand setKubeSecret to accept an arbitrary number of filed/value pairs

--- nix/lib/setup-secrets.nix
+++ nix/lib/setup-secrets.nix
@@ -20,9 +20,16 @@
           kubectl -n "$namespace" get secret "$name" -ogo-template="{{.data.$field | base64decode}}";
         }
         setKubeSecret() {
-          local namespace=$1 name=$2 field=$3 value=$4
-          kubectl create secret generic --dry-run=client -oyaml -n "$namespace" "$name" \
-            --from-literal=$field="$value" | \
+          local namespace=$1 name=$2 field value args=()
+          shift; shift
+          while [[ $# -gt 0 ]]; do
+            field=$1
+            value=$2
+            shift; shift || { printf "Uneven number of arguments\n" >&2; return 1; }
+            args+=("--from-literal=$field=$(printf "%q" "$value")")
+          done
+          kubectl create secret generic --dry-run=client -oyaml \
+            -n "$namespace" "$name" "''${args[@]}" | \
             kubectl apply -f -;
         }
         ${script}
